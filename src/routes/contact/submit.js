3dc6da93aeb406bd77fd54bdea9a107ef0956d1b
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-19 18:28:44 +0200

    submit the native form data and return to the from page

--- src/routes/contact/submit.js
+++ src/routes/contact/submit.js
@@ -4,11 +4,23 @@ export const get = async ({ url }) => {
 	console.log(url.searchParams.get('message_name'));
 	console.log(url.searchParams.get('message_text'));
 
+	// return {
+	// 	status: 200,
+	// 	body: {
+	// 		message_name: url.searchParams.get('message_name'),
+	// 		message_text: url.searchParams.get('message_text')
+	// 	}
+	// };
+
+	// submit the native form data and return to the from page
 	return {
-		status: 200,
-		body: {
-			name: url.searchParams.get('message_name'),
-			text: url.searchParams.get('message_text')
+		status: 303,
+		headers: {
+			location: '/contact'
 		}
+		// body: {
+		// 	message_name: url.searchParams.get('message_name'),
+		// 	message_text: url.searchParams.get('message_text')
+		// }
 	};
 };
