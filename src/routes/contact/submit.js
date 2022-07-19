e2382cbfa7af58ccc7837089a4ec399313bc5adc
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-19 19:03:38 +0200

    PUT request with method parameter ?_method=PUT

--- src/routes/contact/submit.js
+++ src/routes/contact/submit.js
@@ -1,5 +1,5 @@
 export const get = async ({ url }) => {
-	console.log('get request received');
+	console.log('GET request received');
 	//
 	// get search parameters from a native form method get request
 	console.log(url.searchParams.get('message_name'));
@@ -24,7 +24,38 @@ export const get = async ({ url }) => {
 };
 
 export const post = async ({ request }) => {
-	console.log('post request received');
+	console.log('POST request received');
+	//
+	// get formData
+	const form = await request.formData();
+
+	// https://youtu.be/nmX-utfgeK4?t=403 !!!
+	const messageName = form.has('message_name') ? form.get('message_name').toString() : '';
+	console.log(messageName);
+
+	const messageText = form.has('message_text') ? form.get('message_text').toString() : '';
+	console.log(messageText);
+
+	// submit the native form data and return to the from page
+	// return {
+	// 	status: 303,
+	// 	headers: {
+	// 		location: '/contact'
+	// 	}
+	// };
+
+	return {
+		status: 200,
+		body: {
+			name: form.has('message_name') ? form.get('message_name').toString() : '',
+			message: form.has('message_text') ? form.get('message_text').toString() : ''
+		}
+	};
+};
+
+// override method - <form class="py-8" action="/contact/submit?_method=PUT" method="POST">
+export const put = async ({ request }) => {
+	console.log('PUT request received');
 	//
 	// get formData
 	const form = await request.formData();
