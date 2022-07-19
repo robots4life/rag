0848d0bf03589c2428593c367a7898a17815e2ab
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-19 16:57:26 +0200

    swap order of form and each message markup

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -7,35 +7,8 @@
 
 <!-- {JSON.stringify(messages, null, 2)} -->
 
-<h1 class="text-8xl text-center my-8">Contact Us Now</h1>
+<h1 class="text-8xl text-center my-8">Send Your Messge</h1>
 <hr />
-
-{#if messages}
-	<div class="w-full text-2xl text-center py-8">
-		<h2 class="text-6xl">Messages</h2>
-		{#each messages as message, index}
-			<div class="text-2xl py-8">
-				<p class="border">{index + 1}</p>
-				<div class="flex flex-row py-2">
-					<p class="basis-1/2 border">ID :</p>
-					<p class="basis-1/2 border">{message.id}</p>
-				</div>
-				<div class="flex flex-row py-2">
-					<p class="basis-1/2 border">Text :</p>
-					<p class="basis-1/2 border">{message.text}</p>
-				</div>
-				<div class="flex flex-row py-2">
-					<p class="basis-1/2 border">DateTime :</p>
-					<p class="basis-1/2 border">{message.submitDateTimeUnix}</p>
-				</div>
-				<hr />
-			</div>
-		{/each}
-	</div>
-{/if}
-
-<!-- <h1 class="text-8xl text-center my-8">Send Your Messge</h1>
-<hr /> -->
 
 <div class="w-full text-2xl text-center py-8">
 	<form class="py-8">
@@ -69,3 +42,30 @@
 		</div>
 	</form>
 </div>
+
+<!-- <h1 class="text-8xl text-center my-8">Contact Us Now</h1>
+<hr /> -->
+
+{#if messages}
+	<div class="w-full text-2xl text-center py-8">
+		<h2 class="text-6xl">Messages</h2>
+		{#each messages as message, index}
+			<div class="text-2xl py-8">
+				<p class="border">{index + 1}</p>
+				<div class="flex flex-row py-2">
+					<p class="basis-1/2 border">ID :</p>
+					<p class="basis-1/2 border">{message.id}</p>
+				</div>
+				<div class="flex flex-row py-2">
+					<p class="basis-1/2 border">Text :</p>
+					<p class="basis-1/2 border">{message.text}</p>
+				</div>
+				<div class="flex flex-row py-2">
+					<p class="basis-1/2 border">DateTime :</p>
+					<p class="basis-1/2 border">{message.submitDateTimeUnix}</p>
+				</div>
+				<hr />
+			</div>
+		{/each}
+	</div>
+{/if}
