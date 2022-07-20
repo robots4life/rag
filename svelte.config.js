99d66ebd7f2a267b7443269d3d3d82740f3a23d7
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-20 15:58:15 +0200

    sveltekit config changes - build shows forbidden on reload of page - possible nginx conf missing

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,36 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		// adapter: adapter()
+		//
+		// adapter
+		// https://github.com/sveltejs/kit/tree/master/packages/adapter-static
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build'
+			// fallback: null,
+			// precompress: false
+		}),
+		//
+		// prerender
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			//
+			default: true
+			// contact index.js post request : Cannot prerender pages that have endpoints with mutative methods
+			// so turning off for now
+		},
+		//
+		// See https://kit.svelte.dev/docs/configuration#methodoverride
+		//
+		methodOverride: {
+			allowed: ['PUT', 'DELETE']
+		}
 	}
 };
 
