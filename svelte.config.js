8734ab86d0bb0b4840ffb26c56877a8a9e42c498
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-21 16:26:05 +0200

    set sveltekit config to adapter auto - no POST, PUT, DEL endpoints in static builds

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,31 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter: adapter()
+		adapter: adapter(),
 		//
 		// adapter
 		// https://github.com/sveltejs/kit/tree/master/packages/adapter-static
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build'
-			// fallback: null,
-			// precompress: false
-		}),
-		//
-		// prerender
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			//
-			default: true
-			// contact index.js post request : Cannot prerender pages that have endpoints with mutative methods
-			// so turning off for now
-		},
+		// adapter: adapter({
+		// 	// default options are shown
+		// 	pages: 'build',
+		// 	assets: 'build',;
+		// 	trailingSlash: 'always'
+		// 	// fallback: null,
+		// 	// precompress: false
+		// }),
+		// //
+		// // prerender
+		// prerender: {
+		// 	// This can be false if you're using a fallback (i.e. SPA mode)
+		// 	//
+		// 	default: true
+		// 	// contact index.js post request : Cannot prerender pages that have endpoints with mutative methods
+		// 	// so turning off for now
+		// },
 		//
 		// See https://kit.svelte.dev/docs/configuration#methodoverride
 		//
