be3955eb2a18b17d5cbbf30659d809f4e35116ca
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-26 20:04:04 +0200

    svelte config change to adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,15 +17,17 @@ const config = {
 		// 	// fallback: null,
 		// 	// precompress: false
 		// }),
+
 		// //
 		// prerender
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			//
-			default: true
-			// contact index.js post request : Cannot prerender pages that have endpoints with mutative methods
-			// so turning off for now
-		},
+		// prerender: {
+		// 	// This can be false if you're using a fallback (i.e. SPA mode)
+		// 	//
+		// 	default: true
+		// 	// contact index.js post request : Cannot prerender pages that have endpoints with mutative methods
+		// 	// so turning off for now
+		// },
+
 		//
 		// See https://kit.svelte.dev/docs/configuration#methodoverride
 		//
