14e1bd6ccdcd4f612574172784b97bf6be31f658
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-26 20:16:11 +0200

    commit Picture component

---
+++ src/components/Picture.svelte
@@ -0,0 +1,51 @@
+<script>
+	export let member;
+	// console.log(member);
+
+	export const classes = '';
+	// console.log(classes);
+
+	const steps = [
+		8000, 6454, 6453, 2560, 2048, 1920, 1600, 1440, 1280, 1140, 1024, 960, 900, 800, 768, 640,
+		525, 425, 320, 240, 180, 120
+	];
+
+	// export let asset;
+	const srcSetWep = steps.flatMap((element) => {
+		// return member.picture.width >= element ? `${element} : true` : `${element} : false`;
+		return member.picture.width > element
+			? `https://media.graphassets.com/resize=width:` +
+					element +
+					`/output=format:webp/` +
+					member.picture.handle +
+					` ` +
+					element +
+					`w`
+			: [];
+	});
+	// console.log(srcSetWep);
+
+	const srcSet = steps.flatMap((element) => {
+		// return member.picture.width >= element ? `${element} : true` : `${element} : false`;
+		return member.picture.width > element
+			? `https://media.graphassets.com/resize=width:` +
+					element +
+					`/` +
+					member.picture.handle +
+					` ` +
+					element +
+					`w`
+			: [];
+	});
+	// console.log(srcSet);
+</script>
+
+<!-- {JSON.stringify(picture, null, 2)} -->
+
+<picture>
+	<source sizes="" srcset="{srcSetWep}" type="image/webp" />
+
+	<!-- <img style="max-width: 100px; max-height: auto;" src="{picture.url}" alt="" sizes="" srcset="" /> -->
+
+	<img class="{classes}" src="{member.picture.url}" alt="" sizes="" srcset="{srcSet}" />
+</picture>
