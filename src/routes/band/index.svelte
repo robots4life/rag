55e4d9e2e9a3a19766174d64e9db98c32dd1d156
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-26 15:54:25 +0200

    picture styles

--- src/routes/band/index.svelte
+++ src/routes/band/index.svelte
@@ -27,19 +27,23 @@
 				<p class="font-bold basis-1/2 border">Description</p>
 				<p class="basis-1/2 border">{member.description}</p>
 			</div>
-			<div class="flex flex-col border-4 border-green-600 py-2">
+			<div class="flex flex-col border py-2">
 				<p class="font-bold">Instrument</p>
-				<div class="flex justify-center border-4 border-orange-600 py-2">
+				<div class="flex justify-center border py-2">
 					<p class="basis-2/5 border">{member.instrument[0]}</p>
 					{#if member.instrument[1]}
 						<p class="basis-2/5 border">{member.instrument[1]}</p>
 					{/if}
 				</div>
 			</div>
-			<div class="flex flex-col border-4 border-green-600 py-2">
+			<div class="flex flex-col border py-2">
 				<p class="font-bold">Picture</p>
-				<div class="flex justify-center border-4 border-orange-600 py-2">
-					<img class="basis-1/2 border" src="{member.picture.url}" alt="" />
+				<div class="flex justify-center border py-2">
+					<img
+						class="basis-1/2 border-8 border-slate-800"
+						src="{member.picture.url}"
+						alt=""
+					/>
 				</div>
 			</div>
 		{/each}
