3ac0705728d126230915faeafe982fb61db837e1
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-27 15:38:10 +0200

    cleanup

--- src/components/Picture.svelte
+++ src/components/Picture.svelte
@@ -39,31 +39,30 @@
 	});
 	// console.log(srcSet);
 
+	// https://dev.opera.com/articles/responsive-images/
+	// sizes dpi mime
 	/*
-https://dev.opera.com/articles/responsive-images/
-sizes dpi mime
-
-<picture>
-<source
-	sizes="(min-width: 640px) 60vw, 100vw"
-	srcset="opera-200.webp 200w,
-			opera-400.webp 400w,
-			opera-800.webp 800w,
-			opera-1200.webp 1200w,
-			opera-1600.webp 1600w,
-			opera-2000.webp 2000w"
-	type="image/webp">
-<img
-	src="opera-400.jpg" alt="The Oslo Opera House"
-	sizes="(min-width: 640px) 60vw, 100vw"
-	srcset="opera-200.jpg 200w,
-			opera-400.jpg 400w,
-			opera-800.jpg 800w,
-			opera-1200.jpg 1200w,
-			opera-1600.jpg 1600w,
-			opera-2000.jpg 2000w">
-</picture>
-*/
+	<picture>
+	<source
+		sizes="(min-width: 640px) 60vw, 100vw"
+		srcset="opera-200.webp 200w,
+				opera-400.webp 400w,
+				opera-800.webp 800w,
+				opera-1200.webp 1200w,
+				opera-1600.webp 1600w,
+				opera-2000.webp 2000w"
+		type="image/webp">
+	<img
+		src="opera-400.jpg" alt="The Oslo Opera House"
+		sizes="(min-width: 640px) 60vw, 100vw"
+		srcset="opera-200.jpg 200w,
+				opera-400.jpg 400w,
+				opera-800.jpg 800w,
+				opera-1200.jpg 1200w,
+				opera-1600.jpg 1600w,
+				opera-2000.jpg 2000w">
+	</picture>
+	*/
 </script>
 
 <!-- {JSON.stringify(picture, null, 2)} -->
@@ -74,8 +73,6 @@ sizes dpi mime
 		srcset="{srcSetWep}"
 		type="image/webp"
 	/>
-
-	<!-- <img style="max-width: 100px; max-height: auto;" src="{picture.url}" alt="" sizes="" srcset="" /> -->
 
 	<img
 		loading="lazy"
