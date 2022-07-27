e3fd4d6cb2fb7336714d973ef67c9467757a6928
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-27 15:03:11 +0200

    loading = lazy attribute on img element

--- src/components/Picture.svelte
+++ src/components/Picture.svelte
@@ -38,6 +38,32 @@
 			: [];
 	});
 	// console.log(srcSet);
+
+	/*
+https://dev.opera.com/articles/responsive-images/
+sizes dpi mime
+
+<picture>
+<source
+	sizes="(min-width: 640px) 60vw, 100vw"
+	srcset="opera-200.webp 200w,
+			opera-400.webp 400w,
+			opera-800.webp 800w,
+			opera-1200.webp 1200w,
+			opera-1600.webp 1600w,
+			opera-2000.webp 2000w"
+	type="image/webp">
+<img
+	src="opera-400.jpg" alt="The Oslo Opera House"
+	sizes="(min-width: 640px) 60vw, 100vw"
+	srcset="opera-200.jpg 200w,
+			opera-400.jpg 400w,
+			opera-800.jpg 800w,
+			opera-1200.jpg 1200w,
+			opera-1600.jpg 1600w,
+			opera-2000.jpg 2000w">
+</picture>
+*/
 </script>
 
 <!-- {JSON.stringify(picture, null, 2)} -->
@@ -52,6 +78,7 @@
 	<!-- <img style="max-width: 100px; max-height: auto;" src="{picture.url}" alt="" sizes="" srcset="" /> -->
 
 	<img
+		loading="lazy"
 		class="{classes}"
 		src="{member.picture.url}"
 		alt=""
